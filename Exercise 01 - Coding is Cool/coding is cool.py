d30a0761886523c4188411bc6e0b436d4e049372
abur6164-a11y <abur6164@gmail.com>
2025-11-19 11:49:04 +0400

    coding is cool

--- Exercise 01 - Coding is Cool/coding is cool.py
+++ Exercise 01 - Coding is Cool/coding is cool.py
@@ -1,4 +1,8 @@
+# initialising word1 to "coding"
 word1 ="coding"
+# initialising word2 to "coding"
 word2 = "is"
+# initialising word3 to "coding"
 word3 ="cool"
+# displaying the concatenated string on the screen.
 print(word1 + " " + word2 + " " + word3)
